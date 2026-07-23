6568dcca3c4e6888c3d12fa47282e99d91272495
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-23 22:26:35 +0800

    Delete electron directory

--- electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge } = require('electron');
-
-// 暴露安全的 API 给渲染进程
-contextBridge.exposeInMainWorld('electronAPI', {
-  platform: process.platform,
-  isElectron: true,
-});
-
-console.log('✅ Electron preload script loaded');
