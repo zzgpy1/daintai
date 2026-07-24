ae8edcc6f0466161e20e683ea2367a5b7a151701
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-24 22:01:08 +0800

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,10 +1,6 @@
 const { contextBridge } = require('electron');
 
-// 暴露安全的 API 给渲染进程
 contextBridge.exposeInMainWorld('electronAPI', {
   platform: process.platform,
   isElectron: true,
-  getVersion: () => process.versions.electron,
 });
-
-console.log('✅ Electron preload script loaded');
