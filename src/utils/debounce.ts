2b797d2a92e7c6bd082d68d344b6c7df9715455b
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-25 07:22:37 +0800

    Create debounce.ts

---
+++ src/utils/debounce.ts
@@ -0,0 +1,16 @@
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null = null
+  
+  return function executedFunction(...args: Parameters<T>) {
+    const later = () => {
+      timeout = null
+      func(...args)
+    }
+    
+    if (timeout) clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+  }
+}
