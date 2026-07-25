2056d727e12721fb669491a15676ede14c4ddddf
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-25 23:41:37 +0800

    Create debounce.ts

---
+++ src/utils/debounce.ts
@@ -0,0 +1,43 @@
+// src/utils/debounce.ts
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number,
+  immediate?: boolean
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout | null = null
+  
+  return function executedFunction(...args: Parameters<T>) {
+    const later = () => {
+      timeout = null
+      if (!immediate) func(...args)
+    }
+    
+    const callNow = immediate && !timeout
+    
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    
+    timeout = setTimeout(later, wait)
+    
+    if (callNow) {
+      func(...args)
+    }
+  }
+}
+
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle: boolean = false
+
+  return function(this: any, ...args: Parameters<T>): void {
+    if (!inThrottle) {
+      inThrottle = true
+      setTimeout(() => (inThrottle = false), limit)
+      func.apply(this, args)
+    }
+  }
+}
