e299eb182477422f92792be1641d7896c02ad425
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-25 18:23:29 +0800

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,9 +1,20 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
+// 暴露安全的 API 到渲染进程
 contextBridge.exposeInMainWorld('electronAPI', {
   getAppVersion: () => ipcRenderer.invoke('get-app-version'),
   platform: () => ipcRenderer.invoke('platform'),
+  isPackaged: () => ipcRenderer.invoke('is-packaged'),
   minimize: () => ipcRenderer.send('minimize-window'),
   maximize: () => ipcRenderer.send('maximize-window'),
-  close: () => ipcRenderer.send('close-window')
+  close: () => ipcRenderer.send('close-window'),
+  onUpdateAvailable: (callback) => {
+    ipcRenderer.on('update-available', (_, info) => callback(info))
+  },
+  onUpdateDownloaded: (callback) => {
+    ipcRenderer.on('update-downloaded', (_, info) => callback(info))
+  }
 })
+
+// 发送就绪信号
+ipcRenderer.send('renderer-ready')
