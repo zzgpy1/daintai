c85f2f18db097173315bb2516dbe2c161978cace
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-26 10:45:53 +0800

    Add App.vue with main application structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app" class="min-h-screen bg-ios-light-gray dark:bg-dark-bg">
+    <router-view />
+    <ToastContainer />
+    <UpdateNotification />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useSettingsStore } from '@/stores/settings'
+import ToastContainer from '@/components/common/ToastContainer.vue'
+import UpdateNotification from '@/components/common/UpdateNotification.vue'
+
+const settingsStore = useSettingsStore()
+
+onMounted(() => {
+  settingsStore.init()
+})
+</script>
