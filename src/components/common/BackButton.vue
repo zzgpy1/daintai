51ac7e23263815eb1bf703309ea12dd6b1191e08
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-28 06:42:19 +0800

    Create BackButton.vue

---
+++ src/components/common/BackButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    v-if="show"
+    @click="goBack"
+    class="p-2 rounded-full hover:bg-gray-100 dark:hover:bg-dark-gray transition-colors"
+    :title="$t('common.back')"
+  >
+    <ArrowLeftIcon class="w-5 h-5 text-ios-gray dark:text-dark-secondary" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { ArrowLeftIcon } from '@heroicons/vue/24/outline'
+
+const route = useRoute()
+const router = useRouter()
+
+// 不在首页时显示返回按钮
+const show = computed(() => route.path !== '/')
+
+const goBack = () => {
+  router.push('/')
+}
+</script>
