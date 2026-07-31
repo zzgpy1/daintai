a70c6b9c77b84ba85ff0e9f7831c3d9f016ddd54
zzgpy1 <59162982+zzgpy1@users.noreply.github.com>
2026-07-31 19:27:05 +0800

    Create global.d.ts

---
+++ src/types/global.d.ts
@@ -0,0 +1,12 @@
+export {}
+
+declare global {
+  interface Window {
+    electronAPI?: {
+      installUpdate: () => Promise<void>
+      getVersion: () => Promise<string>
+      onUpdateAvailable: (cb: () => void) => () => void
+      onUpdateDownloaded: (cb: () => void) => () => void
+    }
+  }
+}
